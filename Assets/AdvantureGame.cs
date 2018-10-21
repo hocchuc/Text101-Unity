5abd43281a03fb6bff1149b7d5bd2282ef94d3c0
Howard <howard@vinova.sg>
2018-10-21 21:28:56 +0700

    * add more logic to handle input

--- Assets/AdvantureGame.cs
+++ Assets/AdvantureGame.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -16,6 +17,21 @@ public class AdvantureGame : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        ManageState();
 	}
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextState();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+        }
+      
+        textComponent.text = state.GetStateStory();
+    }
 }
--- Assets/State.cs
+++ Assets/State.cs
@@ -6,13 +6,13 @@ using UnityEngine;
 public class State : ScriptableObject {
 
     [TextArea(10,14)] [SerializeField] string storyText;
-    [SerializeField] State nextState;
+    [SerializeField] State[] nextState;
     public string GetStateStory()
     {
         return storyText;
     }
 
-    public State GetNextState()
+    public State[] GetNextState()
     {
         return nextState;
     }
